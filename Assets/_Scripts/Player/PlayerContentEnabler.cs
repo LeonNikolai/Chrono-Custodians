4e6cd7c8d2f221af2ca2e1cf5147551412f1d3d1
Leon Nikolai Engelsvold <leonnikolai.engelsvold@gmail.com>
2024-08-30 15:54:03 +0200

    Made a sript to enable/disable content depending on if a player is the owned by the local player

---
+++ Assets/_Scripts/Player/PlayerContentEnabler.cs
@@ -0,0 +1,74 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerContentEnabler : NetworkBehaviour
+{
+    [Header("Local Player Enabler")]
+    [SerializeField] private GameObject[] enableGameObjectIfOwnedByLocal;
+    [SerializeField] private Behaviour[] enableBehaviourIfOwnedByLocal;
+    [Header("Local Player Disabler")]
+    [SerializeField] private GameObject[] disableGameObjectIfOwnedByLocal;
+    [SerializeField] private Behaviour[] disableBehaviourIfOwnedByLocal;
+
+    [Header("Remote Player Content")]
+    [SerializeField] private GameObject[] enableGameObjectIfOwnedRemotePlayer;
+    [SerializeField] private Behaviour[] enableBehaviourIfOwnedRemotePlayer;
+    [Header("Remote Player Disabler")]
+    [SerializeField] private GameObject[] disableGameObjectIfOwnedRemotePlayer;
+    [SerializeField] private Behaviour[] disableBehaviourIfOwnedRemotePlayer;
+
+
+    public override void OnNetworkSpawn()
+    {
+        if (OwnerClientId == NetworkManager.Singleton.LocalClientId)
+        {
+            LocalContent();
+        }
+        else
+        {
+            Other();
+        }
+        base.OnNetworkSpawn();
+    }
+
+    private void Other()
+    {
+        foreach (var go in enableGameObjectIfOwnedRemotePlayer)
+        {
+            go.SetActive(true);
+        }
+        foreach (var behaviour in enableBehaviourIfOwnedRemotePlayer)
+        {
+            behaviour.enabled = true;
+        }
+        foreach (var go in disableGameObjectIfOwnedRemotePlayer)
+        {
+            go.SetActive(false);
+        }
+        foreach (var behaviour in disableBehaviourIfOwnedRemotePlayer)
+        {
+            behaviour.enabled = false;
+        }
+    }
+
+    private void LocalContent()
+    {
+        foreach (var go in enableGameObjectIfOwnedByLocal)
+        {
+            go.SetActive(true);
+        }
+        foreach (var behaviour in enableBehaviourIfOwnedByLocal)
+        {
+            behaviour.enabled = true;
+        }
+        foreach (var go in disableGameObjectIfOwnedByLocal)
+        {
+            go.SetActive(false);
+        }
+        foreach (var behaviour in disableBehaviourIfOwnedByLocal)
+        {
+            behaviour.enabled = false;
+        }
+    }
+}
