aecc545b5eca8d930aefbb50a175af75267ac540
Leon Nikolai Engelsvold <leonnikolai.engelsvold@gmail.com>
2024-09-06 16:29:54 +0200

    Hud stuff

--- Assets/_Scripts/UI/Hud/Hud.cs
+++ Assets/_Scripts/UI/Hud/Hud.cs
@@ -1,8 +1,39 @@
+using System.ComponentModel;
 using UnityEngine;
 /// <summary>
 /// This class is responsible for the HUD of the game.
 /// </summary>
 public class Hud : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] UiBar _healthBar;
+    [SerializeField] UiBar _staminaBar;
+    [SerializeField] UiBar _shieldBar;
+    [SerializeField] CanvasGroup _hudRoot;
 
+    public static float Opacity
+    {
+        set => instance._hudRoot.alpha = value;
+    }
+    public static float Health
+    {
+        set => instance._healthBar.Progress = value;
+    }
+
+    public static float Stamina
+    {
+        set => instance._staminaBar.Progress = value;
+    }
+
+    public static Hud instance = null;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        if (instance == this) instance = null;
+    }
 }
