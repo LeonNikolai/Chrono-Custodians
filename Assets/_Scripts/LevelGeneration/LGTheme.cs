241af7998afd4d434af4e371cb2ea152f5d5ff3f
dtfe <jonas@jerner.no>
2024-09-24 12:20:52 +0200

    LevelGeneration Stuff

---
+++ Assets/_Scripts/LevelGeneration/LGTheme.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "LGTheme", menuName = "Level Generation/Theme", order = 1)]
+public class LGTheme : ScriptableObject
+{
+    [SerializeField] private List<GameObject> prefabs;
+    [SerializeField] private GameObject entrancePrefab;
+
+    // Optionally, you can add methods to interact with the list
+    public List<GameObject> Rooms => prefabs;
+    public GameObject Entrance => entrancePrefab;
+}
